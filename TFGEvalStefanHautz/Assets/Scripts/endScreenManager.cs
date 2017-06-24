0ee57fe82bb92eb969485f49f19cb7f526edd293
Stefan Hautz <hautzstefan292@gmail.com>
2017-06-24 02:24:02 +0100

    Todo el proyecto
    Añadido todo el proyecto de la versión de evaluación.

---
+++ TFGEvalStefanHautz/Assets/Scripts/endScreenManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+public class endScreenManager : MonoBehaviour
+{
+    public Text scores, starNumber;
+
+    private void Start()
+    {
+        scores.text = "Tiempo total: " + String.Format("{0:0.00}", PlayerPrefs.GetFloat("totalTime")) + "\t Fallos totales: " + PlayerPrefs.GetInt("totalFails");
+        starNumber.text = PlayerPrefs.GetInt("totalStars").ToString();
+        sendMail();
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void restart() {
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene("intro");
+    }
+
+    void sendMail()
+    {
+        MailMessage mail = new MailMessage();
+
+        mail.From = new MailAddress(PlayerPrefs.GetString("fromAddress"));
+        mail.To.Add(PlayerPrefs.GetString("toAddress"));
+        mail.Subject = "Puntuaciones de " + PlayerPrefs.GetString("playerName");
+        mail.Body = "Tiempo Fase 1: " + String.Format("{0:0.00}", PlayerPrefs.GetFloat("timeLevel1")) + "\t Fallos Fase 1: " + PlayerPrefs.GetInt("failsLevel1") +
+            "\nTiempo Fase 2: " + String.Format("{0:0.00}", PlayerPrefs.GetFloat("timeLevel2")) + "\t Fallos Fase 2: " + PlayerPrefs.GetInt("failsLevel2") +
+            "\nTiempo Fase 3: " + String.Format("{0:0.00}", PlayerPrefs.GetFloat("timeLevel3")) + "\t Fallos Fase 3: " + PlayerPrefs.GetInt("failsLevel3") +
+            "\nTiempo Fase 4: " + String.Format("{0:0.00}", PlayerPrefs.GetFloat("timeLevel4")) + "\t Fallos Fase 4: " + PlayerPrefs.GetInt("failsLevel4") +
+            "\nTiempo Fase 5: " + String.Format("{0:0.00}", PlayerPrefs.GetFloat("timeLevel5")) + "\t Fallos Fase 5: " + PlayerPrefs.GetInt("failsLevel5") +
+            "\nTiempo Fase 6: " + String.Format("{0:0.00}", PlayerPrefs.GetFloat("timeLevel6")) + "\t Fallos Fase 6: " + PlayerPrefs.GetInt("failsLevel6") +
+            "\nTiempo Fase 7.1: " + String.Format("{0:0.00}", PlayerPrefs.GetFloat("timeLevel7")) + "\t Fallos Fase 7.1: " + PlayerPrefs.GetInt("failsLevel7") +
+            "\nTiempo Fase 7.2: " + String.Format("{0:0.00}", PlayerPrefs.GetFloat("timeLevel8")) + "\t Fallos Fase 7.2: " + PlayerPrefs.GetInt("failsLevel8") +
+            "\nTiempo total: " + String.Format("{0:0.00}", PlayerPrefs.GetFloat("totalTime")) + "\t Fallos totales: " + PlayerPrefs.GetInt("totalFails");
+
+        SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+        smtpServer.Port = 587;
+        smtpServer.Credentials = new System.Net.NetworkCredential(PlayerPrefs.GetString("fromAddress"), PlayerPrefs.GetString("playerPassword")) as ICredentialsByHost;
+        smtpServer.EnableSsl = true;
+        ServicePointManager.ServerCertificateValidationCallback =
+            delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+            { return true; };
+        smtpServer.Send(mail);
+        Debug.Log("success");
+
+    }
+}
---
+++ TFGEvalStefanHautz/Assets/Scripts/introScreenManager.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+
+public class introScreenManager : MonoBehaviour {
+
+    public Button continueButt, beginButt;
+    public InputField correoFrom, correoTo, nombre, password;
+    public GameObject panelInfo, panelAuthors, panelAbout, animacion;
+
+
+    public void Start()
+    {
+        panelAbout.SetActive(false);
+        panelAuthors.SetActive(false);
+        panelInfo.SetActive(false);
+    }
+
+    public void StartG(string name)
+    {
+        //Application.LoadLevel(Application.loadedLevel);
+        if (IsEmail(correoFrom.text)) {
+            PlayerPrefs.SetString("fromAddress", correoFrom.text);
+            correoFrom.GetComponent<Image>().color = new Color32(0x1A, 0xD0, 0x39, 0xFF);
+        }
+        else {
+            correoFrom.GetComponent<Image>().color = new Color32(0xF6, 0x10, 0x1E, 0xFF);
+        }
+        if (IsEmail(correoTo.text))
+        {
+            PlayerPrefs.SetString("toAddress", correoTo.text);
+            correoTo.GetComponent<Image>().color = new Color32(0x1A, 0xD0, 0x39, 0xFF);
+        }
+        else {
+            correoTo.GetComponent<Image>().color = new Color32(0xF6, 0x10, 0x1E, 0xFF);
+        }
+        if (nombre.text.Length > 6)
+        {
+            PlayerPrefs.SetString("playerName", nombre.text);
+            nombre.GetComponent<Image>().color = new Color32(0x1A, 0xD0, 0x39, 0xFF);
+        }
+        else {
+            nombre.GetComponent<Image>().color = new Color32(0xF6, 0x10, 0x1E, 0xFF);
+        }
+        if (password.text.Length > 6) {
+            PlayerPrefs.SetString("playerPassword", password.text);
+            password.GetComponent<Image>().color = new Color32(0x1A, 0xD0, 0x39, 0xFF);
+        }
+        else {
+            password.GetComponent<Image>().color = new Color32(0xF6, 0x10, 0x1E, 0xFF);
+        }
+        if (PlayerPrefs.HasKey("fromAddress") && PlayerPrefs.HasKey("toAddress") && PlayerPrefs.HasKey("playerName") && PlayerPrefs.HasKey("playerPassword"))
+        {
+            SceneManager.LoadScene(name);
+            PlayerPrefs.SetInt("totalStars", 0);
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void ContinueG() {
+        SceneManager.LoadScene(PlayerPrefs.GetString("savedLevel"));
+    }
+
+    private void Update()
+    {
+        if (!PlayerPrefs.HasKey("savedLevel"))
+        {
+            continueButt.interactable = false;
+            beginButt.interactable = true;
+        }
+        else {
+            continueButt.interactable = true;
+            beginButt.interactable = false;
+        }
+
+    }
+    public const string MatchEmailPattern =
+            @"^(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@"
+            + @"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\."
+              + @"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+            + @"([a-zA-Z]+[\w-]+\.)+[a-zA-Z]{2,4})$";
+
+    public static bool IsEmail(string email)
+    {
+        if (email != null) return Regex.IsMatch(email, MatchEmailPattern);
+        else return false;
+    }
+
+    public void openInfo() {
+        panelInfo.SetActive(true);
+        animacion.SetActive(false);
+    }
+
+    public void closeInfo() {
+        panelInfo.SetActive(false);
+        animacion.SetActive(true);
+    }
+
+    public void openAuthors() {
+        panelAuthors.SetActive(true);
+        animacion.SetActive(false);
+    }
+
+    public void closeAuthors()
+    {
+        panelAuthors.SetActive(false);
+        animacion.SetActive(true);
+    }
+
+    public void openAbout() {
+        panelAbout.SetActive(true);
+        animacion.SetActive(false);
+    }
+
+    public void closeAbout()
+    {
+        panelAbout.SetActive(false);
+        animacion.SetActive(true);
+    }
+
+
+}
